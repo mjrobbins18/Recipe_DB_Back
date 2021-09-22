6f1b5b55fd191d901e930d30ba6b2e66929f5442
mjrobbins18 <maxrobbins88@gmail.com>
2021-09-22 15:04:02 -0400

    almost there

--- recipe_db/recipe_db/settings.py
+++ recipe_db/recipe_db/settings.py
@@ -141,6 +141,3 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-import django_heroku
-django_heroku.settings(locals())
