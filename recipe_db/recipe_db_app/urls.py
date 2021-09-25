bec65a503619d83439436602dc547c71f61dae87
mjrobbins18 <maxrobbins88@gmail.com>
2021-09-25 15:59:10 -0400

    minor styling

--- recipe_db/recipe_db_app/urls.py
+++ recipe_db/recipe_db_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import ObtainTokenPairWithNameView, CustomUserCreate, HelloWorldView, LogoutAndBlacklistRefreshTokenForUserView
+from .views import ObtainTokenPairWithNameView, CustomUserCreate, LogoutAndBlacklistRefreshTokenForUserView
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt import views as jwt_views
 
@@ -14,10 +14,8 @@ urlpatterns = [
     path('ingredient/create', views.IngredientList.as_view(), name='recipe_list'),
     path('user/<str:username>', views.UserDetail.as_view(), name='user_list'),
     path('user/create/', CustomUserCreate.as_view(), name="create_user"),
-    path('token/obtain/', ObtainTokenPairWithNameView.as_view(), name='token_create'),  # override sjwt stock token
-    # path('token/obtain/', jwt_views.TokenObtainPairView.as_view(), name='token_create'),  # override sjwt stock token
+    path('token/obtain/', ObtainTokenPairWithNameView.as_view(), name='token_create'),
     path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
-    path('hello/', HelloWorldView.as_view(), name='hello_world'),
     path('blacklist/', LogoutAndBlacklistRefreshTokenForUserView.as_view(), name = 'blacklist'),
    
 ]
